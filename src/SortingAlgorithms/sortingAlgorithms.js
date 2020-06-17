96b75849a91aa7b8c00fd19c5d4dcfba874a8fc8
kproudler <kevinproudler@gmail.com>
2020-06-16 20:52:26 -0400

    functioning bubbleSort

---
+++ src/SortingAlgorithms/sortingAlgorithms.js
@@ -0,0 +1,17 @@
+export const bubbleSort = array => {
+    let isSorted = false
+
+    while (!isSorted) {
+        isSorted = true;
+
+        for (let i = 0; i < (array.length - 1); i++) {
+            if (array[i] > array[i + 1]) {
+                [array[i], array[i + 1]] = [array[i + 1], array[i]];
+                isSorted = false;
+            }
+        }
+        
+    }
+
+    return array;
+}
